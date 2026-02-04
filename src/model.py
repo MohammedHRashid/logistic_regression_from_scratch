473079c231f686ba91f85719b286eef4a8d6fb16
MohammedHRashid <humayun.r97@gmail.com>
2026-02-04 01:31:33 +0000

    add docstring

--- src/model.py
+++ src/model.py
@@ -2,23 +2,31 @@ import numpy as np
 
 
 def sigmoid(z):
+    """
+    Sigmoid function
+    """
     return 1 / (1 + np.exp(-z))
 
 
 def predict_proba(X, weights, bias):
-    """Return probabilities for logistic regression"""
+    """
+    Return probabilities for logistic regression
+    """
     return sigmoid(np.dot(X, weights) + bias)
 
 
 def predict(X, weights, bias):
-    """Returns integer predictions"""
+    """
+    Returns integer predictions
+    """
     probs = predict_proba(X, weights, bias)
     return (probs >= 0.5).astype(int), probs
 
 
 def compute_loss(y, y_hat, lambda_=0.0, weights=None):
-    """Binary cross-entropy with optional L2 regularization"""
-    m = len(y)
+    """
+    Binary cross-entropy with optional L2 regularization
+    """
     eps = 1e-15  # avoid log(0)
     y_hat = np.clip(y_hat, eps, 1 - eps)
     loss = -np.mean(y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat))
